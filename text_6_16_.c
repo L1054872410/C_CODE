95f1c5198bcba08d8b08cea2300a0db17f109df9
Wishboneflower <1054872410@qq.com>
2023-06-16 18:41:06 +0800

    初学C语言

---
+++ text_6_16_.c
@@ -0,0 +1,50 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//
+//int main() {
+//    printf("     **     \n");
+//    printf("     **     \n");
+//    printf("************\n");
+//    printf("************\n");
+//    printf("    *  *    \n");
+//    printf("    *  *    ");
+//}
+//#include <stdio.h>
+//
+//
+//int main() {
+//    int iq;
+//    while (scanf("%d", &iq) != EOF)
+//    {
+//        if (iq >= 140)
+//            printf("Genius");
+//    }
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//
+//int main() {
+//    printf("I lost my cellphone!");
+//}
+//#include <stdio.h>
+//
+//
+//int main() {
+//    int i;
+//    printf("Name    Age    Gender\n");
+//    for (i = 0; i < 21; i++)
+//        printf("-");
+//    printf("\n");
+//    printf("Jack    18     man");
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main() {
+//    int a = 40, c = 212, num;
+//    num = (-8 + 22) * a - 10 + c / 2;
+//    printf("%d", num);
+//    return 0;
+//}
