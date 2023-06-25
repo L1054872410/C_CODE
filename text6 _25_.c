1936bf74dc6244f992440f3cc31267271bbd4975
Wishboneflower <1054872410@qq.com>
2023-06-25 16:41:36 +0800

    C语言第二次学习

---
+++ text6 _25_.c
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include"stdio.h"
+//int main()
+//{
+//	int num1, num2;
+//	while (scanf("%d%d",&num1, &num2)!=EOF)
+//	{
+//		if (num1 > num2)
+//			printf("%d", num1);
+//		else
+//			printf("%d", num2);
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main() {
+//    int num;
+//    while (scanf("%d", &num) != EOF)
+//    {
+//        if (1 <= num && num <= 100000)
+//        {
+//            if (num % 5 == 0)
+//            {
+//                printf("YES");
+//            }
+//            else
+//            {
+//                printf("NO");
+//            }
+//        }
+//    }
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main() {
+//    int x, y;
+//    while (scanf("%d", &x) != EOF)
+//    {
+//        y = 0;
+//        if (x < 0)
+//            y = 1;
+//        if (x > 0)
+//            y = -1;
+//        printf("%d", y);
+//
+//
+//    }
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main() {
+//    char num = 0;
+//    scanf("%c", &num);
+//
+//
+//    printf("%d", num);
+//
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main() {
+//    int num;
+//    while (scanf("%d", &num) != EOF)
+//    {
+//        if (num % 2 == 0)
+//            printf("Even\n");
+//        else printf("Odd\n");
+//
+//    }
+//    return 0;
+//}
